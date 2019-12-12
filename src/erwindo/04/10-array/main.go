85e261670fba63b4163df472dc3bbf69cf3c9929
Erwindo Sianipar <erwindoq@gmail.com>
2019-12-12 14:10:49 +0700

    first commit in git enigmacamp

---
+++ src/erwindo/04/10-array/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Belajar tipe data array pada Golang\n\n")
+
+	// deklarasi array
+	var nama [4]string
+	nama[0] = "Budi"
+	nama[1] = "Budo"
+	nama[2] = "Buni"
+	nama[3] = "Buno"
+
+	fmt.Println(nama[0])
+	fmt.Println(nama)
+
+	var buah = [4]string{"Apple", "Manggo", "Banana", "Melon"}
+	fmt.Println(buah)
+
+	// inisialisasi array secara vertikal
+	bahasa = [4]string{
+		"indonesia",
+		"inggris"
+	}
+
+	fmt.Println(bahasa)
+}
