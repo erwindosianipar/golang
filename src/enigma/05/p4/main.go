c522d4bee2505ab7df53f2ac6dc0bbcc69cdb1be
Erwindo Sianipar <erwindoq@gmail.com>
2019-12-14 13:39:48 +0700

    latihan array dan perulangan

---
+++ src/enigma/05/p4/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var arrAngka [6][4]int
+
+	for i := 0; i < len(arrAngka); i++ {
+		for k := 0; k < len(arrAngka[0]); k++ {
+			arrAngka[i][k] = k + i*10
+		}
+	}
+
+	fmt.Print(arrAngka)
+}
