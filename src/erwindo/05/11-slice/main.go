5155cfc694b5fb8821a6c7e69e0e72d9069bf572
Erwindo Sianipar <erwindoq@gmail.com>
2019-12-14 09:03:58 +0700

    update latihan slice day 05

--- src/erwindo/05/11-slice/main.go
+++ src/erwindo/05/11-slice/main.go
@@ -1,5 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	var fruitsA = []string{"Apple", "Manggo"}
+	var fruitsB = [2]string{"Kiwi", "Banana"}
+	var fruitsC = [...]string{"Kiwi", "Banana"}
+
+	fmt.Println(fruitsA)
+	fmt.Println(fruitsB)
+	fmt.Println(fruitsC)
 }
