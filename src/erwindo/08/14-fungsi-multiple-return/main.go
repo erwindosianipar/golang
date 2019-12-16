02f3a74cc6c5ff477be9c0ecc70562c087c88e24
Erwindo Sianipar <erwindoq@gmail.com>
2019-12-16 16:25:53 +0700

    latihan multiple return function

--- src/erwindo/08/14-fungsi-multiple-return/main.go
+++ src/erwindo/08/14-fungsi-multiple-return/main.go
@@ -2,12 +2,18 @@ package main
 
 import "fmt"
 
-func multipleReturn(angka int) (int, string) {
-	res := angka * 2
-	return res, "Sukses mengembalikan multiple return"
+func multipleReturn(param int) (int, string) {
+	angka := param * 2
+	kata := "Sukses mengembalikan multiple return"
+	return angka, kata
+}
+
+func multiReturn() (nama string, usia int) {
+	return
 }
 
 func main() {
 
-	fmt.Println(multipleReturn(2))
+	//fmt.Println(multipleReturn(2))
+	fmt.Println(multiReturn())
 }
