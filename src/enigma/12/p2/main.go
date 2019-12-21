b1ec4d3fd2871c2449a8a816f508866466a61635
Erwindo Sianipar <erwindoq@gmail.com>
2019-12-21 11:41:55 +0700

    Belajar concuren pada golang week2 day 06

---
+++ src/enigma/12/p2/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func cetak(str string, delay int) {
+	for i := 0; i < 5; i++ {
+		fmt.Println(str)
+		time.Sleep(time.Duration(delay))
+	}
+}
+
+func main() {
+	cetak("1", 100)
+	cetak("2", 300)
+	cetak("3", 100)
+	cetak("4", 100)
+}
