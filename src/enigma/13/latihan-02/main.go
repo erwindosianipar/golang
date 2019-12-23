f8e0ba016499243917458807de956e12cfd85b29
Erwindo Sianipar <erwindoq@gmail.com>
2019-12-23 17:52:24 +0700

    update latihan-02 week3 day 01

--- src/enigma/13/latihan-02/main.go
+++ src/enigma/13/latihan-02/main.go
@@ -4,16 +4,32 @@ import "fmt"
 
 var pembilang = [12]string{"", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas"}
 
-var angka int = 909
+var angka int = 190
 
 func main() {
+	terbilang(angka)
+}
+
+func terbilang(angka int) {
 	if angka < 12 {
 		fmt.Println(pembilang[angka])
 	} else if angka < 20 {
-		fmt.Println(pembilang[angka-10], "belas")
+		fmt.Println(pembilang[angka-10], " belas")
 	} else if angka < 100 {
-		fmt.Println(pembilang[angka/10], "puluh", pembilang[angka%10])
+		fmt.Println(pembilang[angka/10], " puluh", pembilang[angka%10])
+	} else if angka < 200 {
+		fmt.Println(" seratus", pembilang[angka-100])
 	} else if angka < 1000 {
-		fmt.Println(pembilang[angka/100], "ratus", pembilang[angka%10])
+		fmt.Println(pembilang[angka/100], " ratus", pembilang[angka%100])
+	} else if angka < 2000 {
+		fmt.Println(" seribu", pembilang[angka-1000])
+	} else if angka < 1000000 {
+		fmt.Println(pembilang[angka/1000], " ribu", pembilang[angka%1000])
+	} else if angka < 1000000000 {
+		fmt.Println(pembilang[angka/1000000], " juta", pembilang[angka%1000000])
+	} else if angka < 1000000000000 {
+		fmt.Println(pembilang[angka/1000000000], " milyar", pembilang[angka%1000000000])
+	} else if angka < 1000000000000000 {
+		fmt.Println(pembilang[angka/1000000000000], " trilyun", pembilang[angka%1000000000000])
 	}
 }
